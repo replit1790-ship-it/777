04d77103c689a70da9b53a644d14bdef52612ccf
replit1790-ship-it <replit1790@gmail.com>
2026-01-08 17:51:28 +0300

    Create handlers package initialization file

---
+++ handlers/__init__.py
@@ -0,0 +1,9 @@
+"""
+Handlers package initialization.
+
+This package contains various handler modules for processing different types of requests and events.
+"""
+
+# Package initialization
+__version__ = "1.0.0"
+__all__ = []
